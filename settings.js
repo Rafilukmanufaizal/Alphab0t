9b4be88e5909b7e16fcc853ce7e02c009a9e2660
Rafilukmanufaizal <109738595+Rafilukmanufaizal@users.noreply.github.com>
2022-07-22 19:36:03 +0700

    setting.js

--- settings.js
+++ settings.js
@@ -21,16 +21,16 @@ global.auto_welcome_msg = true
 global.auto_leave_msg = true
 global.prefix = "."
 global.language = "ind"
-global.ownerNumberr = "62887435047326"
-global.ownerbot = ['62887435047326@s.whatsapp.net','918156874290@s.whatsapp.net'] // .owner
-global.ownername = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×"
-global.botname = "Alphabot"
+global.ownerNumberr = "6285600380823"
+global.ownerbot = ['6285600380823@s.whatsapp.net','6285600380823@s.whatsapp.net'] // .owner
+global.ownername = "𝙵𝚊𝚒𝚜𝚊𝚕𝚕"
+global.botname = "𝙽𝚎𝚘-𝙱𝚘𝚝𝚉"
 global.thumb = "thumbnail_1.jpg"
 global.thumbnail ="thumbnail_2_1.jpg"
 global.fakethumb ="thumbnail_1.jpg"
 global.donasi ="donasi.jpg"
 global.captionpay ="Scan untuk melakukan pembayaran"
-global.fake = "「  ALPHABOT  」\nCreated By ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×"
+global.fake = "「  𝙽𝙴𝙾𝙱𝙾𝚃𝚉  」\ncreated By Faisall"
 global.alphaVersion = [2, 2143, 3] //WhatsApp web version
 
 /* Available type menu
